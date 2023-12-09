fdf37dd5429881a7c4069e6fbcec2ae96d2a9e6a
Tim2313 <120585242+Tim2313@users.noreply.github.com>
2023-12-09 19:31:39 +0200

    [CreatingJarFileScript] Add bash script to create jar file

---
+++ buildJar.bash
@@ -0,0 +1,19 @@
+FILE=out
+MANIFEST_FILE=src/main/resources/MANIFEST.MF
+SOURCE_FILES=src/main/java/*
+
+if [ -e "$FILE" ] 
+then
+	rm -rf $FILE/*
+else 
+	mkdir $FILE
+fi
+
+find ./src/ -type f -name "*.java" > sources.txt
+javac -d ./out/ @sources.txt
+rm sources.txt
+
+cd $FILE
+
+jar cfm TestJar.jar ../$MANIFEST_FILE ./*  ../$SOURCE_FILES
+ 
